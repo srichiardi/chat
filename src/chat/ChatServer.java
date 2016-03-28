ddaa15e7637c04ad4544bbebfa6c06b6298ba52c
stefano <s.richiardi@gmail.com>
2016-03-28 21:31:41 +0100

    implemented server scanning

--- src/chat/ChatServer.java
+++ src/chat/ChatServer.java
@@ -6,20 +6,46 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class ChatServer
 {
-	private Map < String , String > clientsRegistry = new HashMap < String , String >();
+	private RemoteList clientsRegistry = new RemoteListImpl();
 
 	// simple constructor
 	public ChatServer () throws RemoteException, MalformedURLException
 	{
-		RemoteList srv = new RemoteListImpl();
-    	Naming.rebind("rmi://localhost/listserver", srv);
+    	Naming.rebind("rmi://localhost/listserver", this.clientsRegistry);
+	}
+	
+	public Map<String, String> getMap() throws RemoteException
+	{
+		return this.clientsRegistry.getList();
+	}
+	
+	public void dropClient(String clientName) throws RemoteException
+	{
+		this.clientsRegistry.removeClient(clientName);
 	}
 	
     public static void main(String args[]) throws Exception
     {
-    	new ChatServer();
+    	ChatServer srv = new ChatServer();
+    	for(;;)
+    	{
+    		Set<String> clientSet = srv.getMap().keySet();
+    		for (String key : clientSet)
+    		{
+    			try {
+    				String rcpntUri = srv.getMap().get(key);
+    				Message rcpntSrv = (Message)Naming.lookup(rcpntUri);
+    			} catch (RemoteException e) {
+    				// assuming the exception is caused by the client offline
+    				srv.dropClient(key);
+    			}
+    		}
+    		// pause the scanning process for 10 seconds
+			Thread.sleep(10000);
+    	}
     }
 }
--- src/chat/ClientUI.java
+++ src/chat/ClientUI.java
@@ -17,6 +17,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
+
 public class ClientUI
 {
 	private String clientName;
